d848d826c1f760c1ad46bd705a9a73843a762826
android <wendyvanessa.am@gmail.com>
2018-09-08 13:10:15 -0500

    Creacion

---
+++ app/src/main/java/com/example/android/turisapp/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.turisapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
